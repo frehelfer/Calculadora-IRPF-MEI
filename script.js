08ad1f6f3786bdd46cf44dad7c0c3270c13ff25b
frehelfer <fre.helfer@gmail.com>
2022-08-14 10:28:29 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// DOM Elements
+const serviços = document.querySelector('#serviços');
+const comércio = document.querySelector('#comércio');
+const transporte = document.querySelector('#transporte');
+
+const despesas = document.querySelector('#despesas');
+
+const receitaBrutaApurado = document.querySelector('#receita-bruta-apurado');
+const lucroApurado = document.querySelector('#lucro-apurado');
+const lucroIsento = document.querySelector('#lucro-isento');
+const lucroTributável = document.querySelector('#lucro-tributável');
+
+// MASKS Setup
+const masks = {
+	money(value) {
+		const cleanValue = +value.replace(/\D+/g, '');
+		const options = { style: 'currency', currency: 'BRL' };
+		return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100);
+	},
+};
+
+document.querySelectorAll('input').forEach(($input) => {
+	const field = $input.dataset.js;
+
+	$input.addEventListener(
+		'input',
+		(e) => {
+			// Mask the value from input fields to BRL
+			e.target.value = masks[field](e.target.value);
+
+			// Get the value from input field
+			const serviçosValue = +serviços.value.replace(/\D+/g, '') / 100;
+			const comércioValue = +comércio.value.replace(/\D+/g, '') / 100;
+			const transporteValue = +transporte.value.replace(/\D+/g, '') / 100;
+			const despesasValue = +despesas.value.replace(/\D+/g, '') / 100;
+
+			const options = { style: 'currency', currency: 'BRL' };
+
+			//Receita Bruta Anual
+			const receitaBrutaApuradoValue = serviçosValue + comércioValue + transporteValue;
+
+			receitaBrutaApurado.innerText = Intl.NumberFormat('pt-br', options).format(
+				receitaBrutaApuradoValue
+			);
+
+			// Lucro Apurado
+			const lucroApuradoValue = receitaBrutaApuradoValue - despesasValue;
+
+			if (lucroApuradoValue < 0) {
+				lucroApurado.innerText = 'R$ 0,00';
+			} else {
+				lucroApurado.innerText = Intl.NumberFormat('pt-br', options).format(lucroApuradoValue);
+			}
+
+			// Lucro Isento
+			const serviçosIsento = serviçosValue * 0.32;
+			const comércioIsento = comércioValue * 0.08;
+			const transporteIsento = transporteValue * 0.16;
+
+			const lucroIsentoValue = serviçosIsento + comércioIsento + transporteIsento;
+
+			lucroIsento.innerText = Intl.NumberFormat('pt-br', options).format(lucroIsentoValue);
+
+			// Lucro Tributável
+			const lucroTributavelValue = lucroApuradoValue - lucroIsentoValue;
+
+			if (lucroTributavelValue < 0) {
+				lucroTributável.innerText = 'R$ 0,00';
+			} else {
+				lucroTributável.innerText = Intl.NumberFormat('pt-br', options).format(
+					lucroTributavelValue
+				);
+			}
+		},
+		false
+	);
+});
