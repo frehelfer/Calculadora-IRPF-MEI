816263cb592d3ebb360ed392aa98198c1d684a06
frehelfer <fre.helfer@gmail.com>
2022-08-25 16:57:36 -0300

    responsive for mobile

--- accordion.js
+++ accordion.js
@@ -10,10 +10,10 @@ accordionHeaders.forEach((accordionHeader) => {
 
 		if (accordionHeader.classList.contains('active-header')) {
 			accordionContent.style.maxHeight = `${height}px`;
-			accordionIcon.style.content = 'url(/images/expand_less_FILL0_wght400_GRAD200_opsz48.svg)';
+			accordionIcon.style.content = 'url(/expand_less_FILL0_wght400_GRAD200_opsz48.svg)';
 		} else {
 			accordionContent.style.maxHeight = '0px';
-			accordionIcon.style.content = 'url(/images/expand_more_FILL0_wght400_GRAD200_opsz48.svg)';
+			accordionIcon.style.content = 'url(/expand_more_FILL0_wght400_GRAD200_opsz48.svg)';
 		}
 	});
 });
